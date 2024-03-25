cd8ba8e7bcf25d1f0109c2c8498953d1c9f26bee
חזן צחיאל <hzachiel@paz-ar.co.il>
2024-03-25 20:53:15 +0200

    sason

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -115,11 +115,11 @@
           </label>
         </div>
       </div>
-      <!-- action="https://server-ravakim-10c1effbda77.herokuapp.com/postFilee" -->
+      <!-- action="http://localhost:3006/postFilee" -->
       <el-upload
         v-if="!AfterUpload"
         class="Uploados"
-        action="http://localhost:3006/postFilee"
+        action="https://server-ravakim-10c1effbda77.herokuapp.com/postFilee"
         multiple
         :on-success="GetPiccher"
       >
